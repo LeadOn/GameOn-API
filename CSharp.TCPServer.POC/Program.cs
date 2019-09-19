506513f45a7a2122c50631bdf37c9b8f55dfd957
Valentin Virot <Valentin.Virot@diiage.org>
2019-09-19 13:20:43 +0200

    Added software

---
+++ CSharp.TCPServer.POC/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Filename: Program.cs
+ * Description: Main code file of the NETGame.CSharp.Server.Core project
+ * Project: .NET Game, first project of the year, Team C
+*/ 
+
+// Adding dependencies
+using System;
+using System.Net;
+using System.Threading;
+
+namespace CSharp.TCPServer.POC
+{
+    class Program
+    {
+        // Constructor
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server Core - .NET Game - Team C");
+            Console.WriteLine("==================================================");
+
+            // Default IP and port
+            var ipAddr = "127.0.0.1";
+            var port = 32101;
+            var tempString = "";
+
+            // if some args were given at soft launch
+            // First arg: IP Address of this server
+            // Second arg: Server port
+            // Default: 127.0.0.1 32101
+            if (args.Length > 0 && args.Length == 2)
+            {
+                Console.WriteLine("Arg 0: " + args[0]);
+                Console.WriteLine("Arg 1: " + args[1]);
+
+                ipAddr = args[0];
+                port = Int32.Parse(args[1]);
+
+                Console.WriteLine("==================================================");
+                Console.WriteLine("Selected @IP: " + IPAddress.Parse(ipAddr) + '\n');
+                Console.WriteLine("Selected port: " + port + '\n');
+                Console.WriteLine("Every settings are set, now launching the server...");
+            }
+
+            // If no args were given
+            else
+            {
+                // Prompt user to type in @IP and port
+                Console.WriteLine("If you want to set it to default, don't type anything, and press ENTER.");
+                Console.Write("Please enter server IP Address (default: localhost): ");
+
+                ipAddr = Console.ReadLine();
+                if(String.IsNullOrWhiteSpace(ipAddr))
+                {
+                    ipAddr = "127.0.0.1";
+                }
+
+                Console.WriteLine("Selected @IP: " + IPAddress.Parse(ipAddr) + '\n');
+
+                Console.Write("Please enter server port (default: 32101): ");
+
+                tempString = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(tempString))
+                {
+                    port = 32101;
+                }
+
+                else
+                {
+                    port = Int32.Parse(tempString);
+                }
+
+                Console.WriteLine("Selected port: " + port + '\n');
+                Console.WriteLine("Every settings are set, now launching the server...");
+            }
+            Console.WriteLine("==================================================\n");
+            
+            // Creating new thread containing TCPServer
+            Thread t = new Thread(delegate ()
+            {
+                // replace the IP with your system IP Address...
+                Server myserver = new Server(ipAddr, port);
+            });
+
+            // Launching thread
+            t.Start();
+        }
+    }
+}
---
+++ CSharp.TCPServer.POC/Server.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * Filename: Server.cs
+ * Description: Main code file of the NETGame.CSharp.Server.Core project
+ * Project: Server code
+*/
+
+// Adding dependencies
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CSharp.TCPServer.POC
+{
+    class Server
+    {
+        // Will listen for TCP Connexion
+        TcpListener server = null;
+
+        // Global Variables
+        IPAddress _ipAddr;
+        int _port;
+        string serverNameInConsole = "Server CORE";
+
+        // Constructor
+        public Server(string ip, int port)
+        {
+            _ipAddr = IPAddress.Parse(ip);
+            _port = port;
+
+            server = new TcpListener(_ipAddr, _port);
+            server.Start();
+
+            printLog(serverNameInConsole, "server successfully started!");
+
+            // Method called by the thread forever
+            StartListener();
+        }
+
+        // Method called when you want to print something in the console
+        private void printLog(string entity, string message)
+        {
+            Console.WriteLine(entity + "> " + message);
+        }
+
+        // Code called all the time by the thread, dedicated by client
+        public void StartListener()
+        {
+            try
+            {
+                // Infinite loop while a client is connected
+                while (true)
+                {
+                    printLog(serverNameInConsole, "Waiting for clients...");
+
+                    // Create a TCPClient when a client will try to connect to the server
+                    TcpClient client = server.AcceptTcpClient();
+
+                    printLog(serverNameInConsole, "new client connected! Initializing thread...");
+
+                    // New thread by client
+                    Thread t = new Thread(new ParameterizedThreadStart(HandleDevice));
+                    t.Start(client);
+                    printLog(serverNameInConsole, "new thread successfully created!");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                printLog(serverNameInConsole, "Error while initializing new thread, error: " + ex.Message);
+            }
+        }
+
+        // Method used by each thread
+        public void HandleDevice(Object obj)
+        {
+            // Local variables
+            var threadID = Thread.CurrentThread.ManagedThreadId;
+            var clientNameInConsole = "Client n°" + threadID;
+
+            try
+            {
+                printLog(clientNameInConsole, "thread initialized");
+
+                TcpClient client = (TcpClient)obj;
+
+                // Reading incoming data from client
+                var stream = client.GetStream();
+                Byte[] bytes = new Byte[102400];
+                StringBuilder myCompleteMessage = new StringBuilder();
+                int nbrOfBytesRead = 0;
+
+                do
+                {
+                    do
+                    {
+                        nbrOfBytesRead = stream.Read(bytes, 0, bytes.Length);
+                        myCompleteMessage.AppendFormat("{0}", Encoding.UTF8.GetString(bytes, 0, nbrOfBytesRead));
+                    }
+                    while (stream.DataAvailable);
+
+                    // Flushing data stream
+                    stream.Flush();
+
+                    string str = threadID.ToString();
+                    Byte[] reply = Encoding.UTF8.GetBytes(str);
+                    stream.Write(reply, 0, reply.Length);
+
+                    // Printing string received by client
+                    printLog(clientNameInConsole, myCompleteMessage.ToString());
+
+                    // Resetting byte array (data received by client)
+                    bytes = new Byte[102400];
+
+                    // Resetting StringBuilder
+                    myCompleteMessage.Clear();
+                } while (true);
+            }
+            catch (Exception ex)
+            {
+                printLog(serverNameInConsole, "client n°" + threadID + " disconnected! Reason: " + ex.Message);
+                Thread.CurrentThread.Interrupt();
+            }
+        }
+    }
+}
