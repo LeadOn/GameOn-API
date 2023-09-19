638d1ecb9ecdd299d86539a10043b76f553b98c9
Valentin Virot <virot.valentin@gmail.com>
2023-09-19 19:53:30 +0200

    Added base Clean Architecture projects

---
+++ YuGames.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="DependencyInjection.cs" company="LeadOn's Corp'">
+// Copyright (c) LeadOn's Corp'. All rights reserved.
+// </copyright>
+
+namespace YuGames.Persistence
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Dependency Injection class.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Adds all persistence dependency injections.
+        /// </summary>
+        /// <param name="services">IServiceCollection object.</param>
+        /// <returns>IServiceCollection.</returns>
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
