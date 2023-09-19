db3c5d449e6a41938bfe58a35db1ca1aad155ab6
Valentin Virot <virot.valentin@gmail.com>
2023-09-19 22:03:48 +0200

    Added Get Seasons

---
+++ YuGames.Application/Seasons/Queries/GetCurrentSeason/GetCurrentSeasonQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="GetCurrentSeasonQueryHandler.cs" company="LeadOn's Corp'">
+// Copyright (c) LeadOn's Corp'. All rights reserved.
+// </copyright>
+
+namespace YuGames.Application.Seasons.Queries.GetCurrentSeason
+{
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+    using YuGames.Application.Common.Interfaces;
+    using YuGames.Domain;
+
+    /// <summary>
+    /// GetCurrentSeasonQueryHandler class.
+    /// </summary>
+    public class GetCurrentSeasonQueryHandler : IRequestHandler<GetCurrentSeasonQuery, Season?>
+    {
+        private readonly IApplicationDbContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetCurrentSeasonQueryHandler"/> class.
+        /// </summary>
+        /// <param name="context">DbContext, injected.</param>
+        public GetCurrentSeasonQueryHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <inheritdoc />
+        public async Task<Season?> Handle(GetCurrentSeasonQuery request, CancellationToken cancellationToken)
+        {
+            return await this.context.Seasons.FirstOrDefaultAsync(x => x.Id == int.Parse(Environment.GetEnvironmentVariable("CURRENT_SEASON") ?? "1"));
+        }
+    }
+}
