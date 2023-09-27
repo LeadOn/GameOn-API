34f69a46ebfa4cd2d1632d24b64e282b1718555f
Valentin Virot <virot.valentin@gmail.com>
2023-09-27 14:04:26 +0200

    Working on global stats

---
+++ YuGames.Application/Stats/Queries/GetGlobalStats/GetGlobalStatsQuery.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="GetGlobalStatsQuery.cs" company="LeadOn's Corp'">
+// Copyright (c) LeadOn's Corp'. All rights reserved.
+// </copyright>
+
+namespace YuGames.Application.Stats.Queries.GetGlobalStats
+{
+    using MediatR;
+    using YuGames.Common.DTOs;
+
+    /// <summary>
+    /// GetGlobalStatsQuery class.
+    /// </summary>
+    public class GetGlobalStatsQuery : IRequest<GlobalStatsDto>
+    {
+    }
+}
