0023a53aeba6f8745411c59a828a83599036768a
Valentin Virot <virot.valentin@gmail.com>
2024-12-19 13:32:14 +0100

    Added route to get active LoL players

---
+++ GameOn.Application/LeagueOfLegends/Summoners/Queries/GetAllLeaguePlayers/GetAllLeaguePlayersQuery.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="GetAllLeaguePlayersQuery.cs" company="LeadOn's Corp'">
+// Copyright (c) LeadOn's Corp'. All rights reserved.
+// </copyright>
+
+namespace GameOn.Application.LeagueOfLegends.Summoners.Queries.GetAllLeaguePlayers
+{
+    using GameOn.Domain;
+    using MediatR;
+
+    /// <summary>
+    /// GetAllLeaguePlayersQuery class.
+    /// </summary>
+    public class GetAllLeaguePlayersQuery : IRequest<IEnumerable<Player>>
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether a player is archived or not.
+        /// </summary>
+        public bool Archived { get; set; } = false;
+    }
+}
