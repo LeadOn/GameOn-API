eb832bc6711ff27430f7cf0ceb3da217521692a6
Valentin Virot <virot.valentin@gmail.com>
2024-12-26 23:07:41 +0100

    Working on game management

--- GameOn.External/RiotGames/Implementations/AccountV1Service.cs
+++ GameOn.External/RiotGames/Implementations/AccountV1Service.cs
@@ -12,26 +12,27 @@ namespace GameOn.External.RiotGames.Implementations
     /// <summary>
     /// AccountV1Service class.
     /// </summary>
-    public class AccountV1Service : HttpServiceBase, IAccountService
+    public class AccountV1Service : IAccountService
     {
-        private readonly HttpClient client;
+        private HttpClient client;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AccountV1Service"/> class.
         /// </summary>
         /// <param name="client"><see cref="HttpClient"/>.</param>
-        public AccountV1Service(HttpClient client)
+        public AccountV1Service()
         {
-            this.client = client;
+            this.client = new HttpClient();
         }
 
         /// <inheritdoc/>
         public async Task<AccountDto> GetAccountPuuid(string tagLine, string nickname, CancellationToken cancellationToken)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, $"https://{Environment.GetEnvironmentVariable("RIOT_GAMES_ACCOUNT_API_ROUTE")}/riot/account/v1/accounts/by-riot-id/{nickname}/{tagLine}?api_key={Environment.GetEnvironmentVariable("RIOT_GAMES_API_KEY")}");
-#pragma warning disable CS8603 // Existence possible d'un retour de référence null.
-            return await RunRequest<AccountDto>(this.client, request, cancellationToken);
-#pragma warning restore CS8603 // Existence possible d'un retour de référence null
+            return new AccountDto();
+//            var request = new HttpRequestMessage(HttpMethod.Get, $"https://{Environment.GetEnvironmentVariable("RIOT_GAMES_ACCOUNT_API_ROUTE")}/riot/account/v1/accounts/by-riot-id/{nickname}/{tagLine}?api_key={Environment.GetEnvironmentVariable("RIOT_GAMES_API_KEY")}");
+//#pragma warning disable CS8603 // Existence possible d'un retour de référence null.
+//            return await RunRequest<AccountDto>(this.client, request, cancellationToken);
+//#pragma warning restore CS8603 // Existence possible d'un retour de référence null
         }
     }
 }
