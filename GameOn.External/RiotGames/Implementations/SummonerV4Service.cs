1760113e156858e26af4b1fa7aaee7ae5be9fcfb
Valentin Virot <virot.valentin@gmail.com>
2024-12-27 02:14:23 +0100

    Reactivated services with HttpClient injection

--- GameOn.External/RiotGames/Implementations/SummonerV4Service.cs
+++ GameOn.External/RiotGames/Implementations/SummonerV4Service.cs
@@ -7,32 +7,30 @@ namespace GameOn.External.RiotGames.Implementations
     using GameOn.External.Common;
     using GameOn.External.RiotGames.Interfaces;
     using GameOn.External.RiotGames.Models.DTOs;
-    using Newtonsoft.Json;
 
     /// <summary>
     /// SummonerV4Service class.
     /// </summary>
-    public class SummonerV4Service : ISummonerService
+    public class SummonerV4Service : HttpServiceBase, ISummonerService
     {
-        private HttpClient client;
+        private readonly HttpClient client;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SummonerV4Service"/> class.
         /// </summary>
         /// <param name="client">HTTP Client.</param>
-        public SummonerV4Service()
+        public SummonerV4Service(HttpClient client)
         {
-            this.client = new HttpClient();
+            this.client = client;
         }
 
         /// <inheritdoc/>
         public async Task<SummonerDto> GetSummonerByPuuid(string puuid, CancellationToken cancellationToken)
         {
-            return new SummonerDto();
-//            var request = new HttpRequestMessage(HttpMethod.Get, $"https://{Environment.GetEnvironmentVariable("RIOT_GAMES_SUMMONER_API_ROUTE")}/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={Environment.GetEnvironmentVariable("RIOT_GAMES_API_KEY")}");
-//#pragma warning disable CS8603 // Existence possible d'un retour de référence null.
-//            return await RunRequest<SummonerDto>(this.client, request, cancellationToken);
-//#pragma warning restore CS8603 // Existence possible d'un retour de référence null.
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://{Environment.GetEnvironmentVariable("RIOT_GAMES_SUMMONER_API_ROUTE")}/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={Environment.GetEnvironmentVariable("RIOT_GAMES_API_KEY")}");
+#pragma warning disable CS8603 // Existence possible d'un retour de référence null.
+            return await RunRequest<SummonerDto>(this.client, request, cancellationToken);
+#pragma warning restore CS8603 // Existence possible d'un retour de référence null.
         }
     }
 }
