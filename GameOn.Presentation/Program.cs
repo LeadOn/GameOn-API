a80f6a917e3aacb2168c5a670db35e76e04a5d23
Valentin Virot <virot.valentin@gmail.com>
2025-04-12 12:41:00 +0200

    Updated package versions

--- GameOn.Presentation/Program.cs
+++ GameOn.Presentation/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddSwaggerGen(c =>
     c.EnableAnnotations();
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
-        Version = "5.0.2",
+        Version = "5.1.0",
         Title = "LeadOn's Corp - GameOn! API",
         Description = "This API goal is to monitor players performance across multiple games.",
         Contact = new Microsoft.OpenApi.Models.OpenApiContact
