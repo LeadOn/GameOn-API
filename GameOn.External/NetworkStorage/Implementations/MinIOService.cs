fc4071e03be1138ccd0a171924f7f5fdcd4894ea
Valentin Virot <virot.valentin@gmail.com>
2025-07-15 20:34:30 +0200

    Now able to query files from a dedicated server! Profile Picture are handled by that.

---
+++ GameOn.External/NetworkStorage/Implementations/MinIOService.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="AccountV1Service.cs" company="LeadOn's Corp'">
+// Copyright (c) LeadOn's Corp'. All rights reserved.
+// </copyright>
+
+namespace GameOn.External.RiotGames.Implementations
+{
+    using GameOn.External.Common;
+    using GameOn.External.RiotGames.Interfaces;
+    using GameOn.External.RiotGames.Models.DTOs;
+
+    /// <summary>
+    /// AccountV1Service class.
+    /// </summary>
+    public class AccountV1Service : HttpServiceBase, IAccountService
+    {
+        private readonly HttpClient client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountV1Service"/> class.
+        /// </summary>
+        /// <param name="client"><see cref="HttpClient"/>.</param>
+        public AccountV1Service(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        /// <inheritdoc/>
+        public async Task<AccountDto> GetAccountPuuid(string tagLine, string nickname, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://{Environment.GetEnvironmentVariable("RIOT_GAMES_ACCOUNT_API_ROUTE")}/riot/account/v1/accounts/by-riot-id/{nickname}/{tagLine}?api_key={Environment.GetEnvironmentVariable("RIOT_GAMES_API_KEY")}");
+#pragma warning disable CS8603 // Existence possible d'un retour de référence null.
+            return await RunRequest<AccountDto>(this.client, request, cancellationToken);
+#pragma warning restore CS8603 // Existence possible d'un retour de référence null
+        }
+    }
+}
