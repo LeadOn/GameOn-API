96c8102d33149cd08e8c64aca1f24f641ce2305c
Valentin Virot <virot.valentin@gmail.com>
2025-07-20 12:41:15 +0200

    Added new tournament logo mecanism

---
+++ GameOn.Common/DTOs/TournamentLogoDto.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ProfilePictureDto.cs" company="LeadOn's Corp'">
+// Copyright (c) LeadOn's Corp'. All rights reserved.
+// </copyright>
+
+namespace GameOn.Common.DTOs
+{
+    using GameOn.Domain;
+
+    /// <summary>
+    /// ProfilePictureDto class.
+    /// </summary>
+    public class ProfilePictureDto
+    {
+        /// <summary>
+        /// Gets or sets File Stream.
+        /// </summary>
+        public Stream? FileStream { get; set; }
+
+        /// <summary>
+        /// Gets or sets File name.
+        /// </summary>
+        public string FileName { get; set; } = string.Empty;
+    }
+}
